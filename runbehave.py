34fa3e5a2761a731d3ba877f11623f65f7520295
horsewithnoname1985 <arnewohletz@gmx.de>
2018-11-30 15:30:44 +0100

    deleted a comment

--- runbehave.py
+++ runbehave.py
@@ -1,7 +1,3 @@
 from behave.__main__ import main as behave_main
-# from behave.configuration import Configuration
-#
-# Configuration.name = "Create new user"
 
-# behave_main(["./features", "-i .+\.feature"])
 behave_main(["./features", "-t @run"])
