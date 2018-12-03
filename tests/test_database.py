9d060368eaed44e23ddf849adfe4b26c3724f0e3
horsewithnoname1985 <arnewohletz@gmx.de>
2018-12-03 16:31:50 +0100

    updated requirements first unit tests pass

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,18 +5,19 @@
 
 import pytest
 import os
+import sqlite3
 from packit_app.database import Database
 
 
 def test_database_connection():
     db = Database()
-    assert type(db.cur) == "sqlite3.Cursor"
-    assert type(db.connection) == "sqlite3.Connection"
-    assert os.path.exists(Database.db_location) == True
+    assert type(db.cur) is sqlite3.Cursor
+    assert type(db.connection) is sqlite3.Connection
+    assert os.path.exists(Database.db_location) is True
 
 
 def test_database_disconnects():
     db = Database()
     db.close_connection()
-    assert db.cur.execute(
-        "") == "sqlite3.ProgrammingError: Cannot operate on a closed database."
+    with pytest.raises(sqlite3.ProgrammingError) as error:
+        db.cur.execute("CREATE TABLE cannot_not_be_created")
