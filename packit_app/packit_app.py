9bf16e0d8f1d697b163dc81542a0cedd80c70ff7
horsewithnoname1985 <arnewohletz@gmx.de>
2018-12-18 11:02:03 +0100

    initial commit added Application class

--- packit_app/packit_app.py
+++ packit_app/packit_app.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from .database import Database
+from .tables import ConcreteTableFactory
+from .elements import User, Gender
+
 """Main module."""
+
+
+class Application:
+
+    database = None
+    user_table = None
+    gender_table = None
+
+    def initialize(self):
+        table_factory = ConcreteTableFactory()
+        self.database = Database()
+        self.user_table = table_factory.create_table(User())
+        self.gender_table = table_factory.create_table(Gender())
